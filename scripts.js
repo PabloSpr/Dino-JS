0f332e506a1855eeb04f46bdeb4dd973854d5ccc
PabloSpr <sw.pablo@gmail.com>
2022-05-17 12:04:16 -0300

    saltar y correr

--- scripts.js
+++ scripts.js
@@ -1,9 +1,19 @@
 const player = document.getElementById("dino");
+let correr = true;
+
+function corriendo() {
+  if (correr) {
+    player.classList.add("dino-corre");
+  }
+}
+corriendo();
 
 document.addEventListener("keydown", handelKey);
 
 function handelKey(ev) {
   if (ev.keyCode === 32) {
+    correr = false;
+    player.classList.remove("dino-corre");
     jump();
   }
 }
@@ -14,4 +24,6 @@ function jump() {
 
 player.addEventListener("animationend", () => {
   player.classList.remove("dino-jump");
+  correr = true;
+  corriendo();
 });
